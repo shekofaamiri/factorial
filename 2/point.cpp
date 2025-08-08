a168db12ca310a2d068edb6a7a996537ad13829b
shekofaamiri <shekofaamiri1@gmail.com>
2025-08-08 22:02:15 +0330

    constructor added

--- 2/point.cpp
+++ 2/point.cpp
@@ -14,3 +14,27 @@ public:
     int get_y();
     void print();
 };
+ Point::Point() {}
+Point::Point (int a, int b)
+{
+    x = a;
+    y = b;
+}
+void Point::set_x(int a) {
+    x=a;
+}
+void Point::set_y(int b){
+    y=b;
+}
+int Point::get_x()
+{
+    return x;
+}
+int Point::get_y()
+{
+    return y;
+}
+void Point::print()
+{
+    cout << x << y << endl;
+}
